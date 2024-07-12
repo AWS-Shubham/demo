0e8cce6d3bea088b5782e091b410be3f700d3687
AWS-Shubham <shubham.dongarwar@smart-solutions.website>
2024-07-12 13:22:14 +0530

    new_update
    new

--- dj/dj/urls.py
+++ dj/dj/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from dj.dj import settings
 from django.conf import settings # new
 from django.conf.urls.static import static #new
 
